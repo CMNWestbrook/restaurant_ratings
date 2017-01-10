8048e92d8eb1ebd94219a66e779da093b3229eae
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-09 18:01:24 -0800

    unfinished attempt to have options either print list, add to list, or quit

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -7,9 +7,31 @@ file_name = sys.argv[1]
 def put_restaurant_and_rating(restaurant_ratings):
     """Adds user inputted restaurant and rating
     """
+
     restaurant = raw_input("restaurant name: ")
     rating = raw_input("rating: ")
     restaurant_ratings[restaurant] = rating
+
+
+def ratings_menu(restaurant_ratings):
+    while True:
+        print "Menu"
+        print "1: See all ratings."
+        print "2: Add a new restaurant and rating."
+        print "3: Exit menu and print ratings."
+        options = raw_input("Make a selection. \n")
+
+        if options == "1":
+            break
+        elif options == "2":
+            restaurant = raw_input("Please input a restaurant name: ")
+            if restaurant == "3":
+                break
+            else:
+                rating = raw_input("rating: ")
+                restaurant_ratings[restaurant] = rating
+        elif options == "3":
+            break
 
 
 def print_ratings(file_name):
